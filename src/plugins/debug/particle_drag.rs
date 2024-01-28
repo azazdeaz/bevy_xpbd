e81136af3d05cd78d5e5a47a93633e6cb021960f
Polgár András <azazdeaz@gmail.com>
2024-01-28 09:10:04 +0100

    Add infinite mass to dragged particles

--- src/plugins/debug/particle_drag.rs
+++ src/plugins/debug/particle_drag.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::{InverseMass, RigidBodyQuery};
+
 pub struct DragParticlePlugin;
 impl Plugin for DragParticlePlugin {
     fn build(&self, app: &mut App) {
@@ -7,9 +9,11 @@ impl Plugin for DragParticlePlugin {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 struct DragInfo {
     id: Entity,
     grab_distance: f32,
+    grab_inverse_mass: InverseMass,
 }
 
 #[derive(Component, Default)]
@@ -24,7 +28,7 @@ impl DragParticle {
 
 fn drag_particles(
     camera_query: Query<(&Camera, &GlobalTransform), With<Camera3d>>,
-    mut particles: Query<(Entity, &mut Transform, &DragParticle)>,
+    mut particles: Query<(RigidBodyQuery, &DragParticle)>,
     buttons: Res<Input<MouseButton>>,
     windows: Query<&Window>,
     mut gizmos: Gizmos,
@@ -46,46 +50,58 @@ fn drag_particles(
 
     if buttons.just_pressed(MouseButton::Left) {
         info!("Clicked");
-        let mut closest: Option<(Entity, f32)> = None;
-        for (id, particle) in particles.into_iter().filter_map(|(id, transform, drag)| {
-            if drag.enabled {
-                Some((id, transform))
-            } else {
-                None
-            }
-        }) {
+        let mut closest: Option<(DragInfo, f32)> = None;
+        for body in particles.into_iter().filter_map(
+            |(body, drag)| {
+                if drag.enabled {
+                    Some(body)
+                } else {
+                    None
+                }
+            },
+        ) {
             // calculate the particle distance from the ray
-            let particle_from_origin = particle.translation - ray.origin;
+            let particle_from_origin = body.position.0 - ray.origin;
             let closest_point_on_ray = ray.direction * particle_from_origin.dot(ray.direction);
             let distance = (particle_from_origin - closest_point_on_ray).length();
             info!("Distance: {}", distance);
             if distance < 0.5 && (closest.is_none() || distance < closest.unwrap().1) {
-                closest = Some((id, distance));
+                closest = Some((
+                    DragInfo {
+                        id: body.entity,
+                        grab_distance: body.position.0.distance(ray.origin),
+                        grab_inverse_mass: *body.inverse_mass,
+                    },
+                    distance,
+                ));
             }
         }
-        if let Some((id, _)) = closest {
-            *drag_state = Some(DragInfo {
-                id,
-                grab_distance: particles
-                    .get(id)
-                    .unwrap()
-                    .1
-                    .translation
-                    .distance(ray.origin),
-            });
+        if let Some((info, _)) = closest.take() {
+            *drag_state = Some(info);
+
+            // Set inverse_mass to 0 to make the dragged body immovable
+            if let Ok((mut body, _)) = particles.get_mut(info.id) {
+                body.inverse_mass.0 = 0.0;
+            }
         }
     } else if buttons.just_released(MouseButton::Left) {
-        *drag_state = None;
+        // Reset drag_state and inverse_mass
+        if let Some(info) = drag_state.take() {
+            if let Ok((mut body, _)) = particles.get_mut(info.id) {
+                body.inverse_mass.0 = info.grab_inverse_mass.0;
+            }
+        }
     }
 
     if let Some(info) = &*drag_state {
-        info!("Dragging {:?}", info.id);
-        let new_pos = ray.origin + ray.direction * info.grab_distance;
-        let mut particle_position = particles.get_mut(info.id).unwrap().1;
-        particle_position.translation = new_pos;
-        gizmos.cuboid(
-            Transform::from_translation(new_pos).with_scale(Vec3::splat(0.1)),
-            Color::PINK,
-        );
+        if let Ok((mut body, _)) = particles.get_mut(info.id) {
+            info!("Dragging {:?}", info.id);
+            let new_pos = ray.origin + ray.direction * info.grab_distance;
+            body.position.0 = new_pos;
+            gizmos.cuboid(
+                Transform::from_translation(new_pos).with_scale(Vec3::splat(0.1)),
+                Color::PINK,
+            );
+        }
     }
 }
